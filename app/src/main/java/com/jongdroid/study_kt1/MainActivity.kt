ecfaae95994bfed932a3ddc715f3c5d21744f23b
jongdroid <qkr7627@gmail.com>
2022-02-14 20:19:26 +0900

    코틀린 학습

---
+++ app/src/main/java/com/jongdroid/study_kt1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jongdroid.study_kt1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//test
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
