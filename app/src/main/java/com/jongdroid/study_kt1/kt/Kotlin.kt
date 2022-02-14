61798013081a14303fbd7b325eb8bdc5b247898e
jongdroid <qkr7627@gmail.com>
2022-02-14 22:06:57 +0900

    01. Variable

---
+++ app/src/main/java/com/jongdroid/study_kt1/kt/Kotlin.kt
@@ -0,0 +1,31 @@
+package com.jongdroid.study_kt1.kt
+
+// 01.  변수
+
+// 변수 선언하는 방법
+// var 또는 val = 상자라고 생각하자
+// var 변수명 상자 = 값
+// Variable / Value
+
+// **상자의 특징**
+// Variable 은 값을 바꿀 수 있다.
+// Value 는 값을 바꿀 수 없다.
+
+// variable 상자
+var name = "코틀린"
+var number = 99
+var score = 99.4
+
+// value 상자
+val maxSpeed = 200
+
+
+
+fun main(args:Array<String>) {
+// maxSpeed = 300 --> value는 값을 바꿀 수 없다.
+
+    //프로그램이 돌때 name에 있는 값을 가져와서 수행한다.
+    println(name)
+
+
+}
