873994812fe3a12aa3bb0f07366d7e9f06044427
jongdroid <qkr7627@gmail.com>
2022-02-14 20:20:01 +0900

    test_2

--- app/src/main/java/com/jongdroid/study_kt1/MainActivity.kt
+++ app/src/main/java/com/jongdroid/study_kt1/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jongdroid.study_kt1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//test
+//test //ㅇㅈㅇㅈㅇ
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
